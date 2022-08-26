f7072725e98ba65ed0b443df8e2328b7df5349b8
Pirai Ojeda (Rio) <87847753+riosol7@users.noreply.github.com>
2022-08-26 16:44:45 -0700

    REST init

--- assignments/src/main/java/wk3/mySQL.java
+++ assignments/src/main/java/wk3/mySQL.java
@@ -10,53 +10,57 @@ import java.sql.SQLException;
 public class mySQL {
 
 	public void start() {
-		
+
 		String url = "jdbc:mysql://localhost:3306/userdb";
 		String user = "root";
 		String pass = "Waka#123196";
-		
+
+		try {
+			// Load the driver
+			Class.forName("com.mysql.cj.jdbc.Driver");
+
 			try {
-				// Load the driver
-				Class.forName("com.mysql.cj.jdbc.Driver");
-				
-				try {
-					// Connect to the db
-					Connection connection = DriverManager.getConnection(url, user, pass);
-					System.out.println("Connection is successful to db " + url + "\n");
-					
-					// OUTER JOIN not working in mySQL?	
-					String query = "SELECT employee.*, department.location FROM userdb.employee CROSS JOIN userdb.department ON department.dept_name = employee.dept;";
-					
-					// Created a prepared statement (for non-leakage), it'll return one statement object
-					PreparedStatement pstmt = connection.prepareStatement(query);
-					
-					ResultSet rs = pstmt.executeQuery();
-					
-					ResultSetMetaData  rsmd = rs.getMetaData(); 
-					
-					int columnsNumber = rsmd.getColumnCount();
-					  
-					while (rs.next()) {
-						for (int i = 1; i <= columnsNumber; i++) {
-					           
-							if (i > 1) System.out.print(" | ");
-							
-					        String columnValue = rs.getString(i);
-					        System.out.print(rsmd.getColumnName(i) + ": " + columnValue);
-					    };
-					
-						System.out.println("");
-					};
-									
-				} catch (SQLException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+				// Connect to the db
+				Connection connection = DriverManager.getConnection(url, user, pass);
+				System.out.println("Connection is successful to db " + url + "\n");
+
+				// OUTER JOIN not working in mySQL?
+				String query = "SELECT employee.*, department.location FROM userdb.employee CROSS JOIN userdb.department ON department.dept_name = employee.dept;";
+
+				// Created a prepared statement (for non-leakage), it'll return one statement
+				// object
+				PreparedStatement pstmt = connection.prepareStatement(query);
+
+				ResultSet rs = pstmt.executeQuery();
+
+				ResultSetMetaData rsmd = rs.getMetaData();
+
+				int columnsNumber = rsmd.getColumnCount();
+
+				while (rs.next()) {
+					for (int i = 1; i <= columnsNumber; i++) {
+
+						if (i > 1)
+							System.out.print(" | ");
+
+						String columnValue = rs.getString(i);
+						System.out.print(rsmd.getColumnName(i) + ": " + columnValue);
+					}
+					;
+
+					System.out.println("");
 				}
-			} catch (ClassNotFoundException e) {
+				;
+
+			} catch (SQLException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 //		};
-		
+
 	}
 }
