6382cf630120a09ed27adc3d1ed05021f9d55e25
Pirai Ojeda (Rio) <87847753+riosol7@users.noreply.github.com>
2022-08-26 18:13:07 -0700

    hw4 init

--- demoREST/src/main/java/com/demo/HelloController.java
+++ demoREST/src/main/java/com/demo/HelloController.java
@@ -1,6 +1,9 @@
 package com.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +12,18 @@ public class HelloController {
 	@GetMapping("/hello")
 	public String HelloWorld() {
 		return "HelloWorld";
+	}
+	
+	//world?variable=value
+	
+	@PostMapping("/world")
+	public Hello helloWorld(@RequestParam String lastName, @RequestBody Hello input) {
+		Hello h = new Hello();
+		h.setFirstName(input.getFirstName() + "Resp");
+		h.setLastName(input.getLastName() + "Resp");
+
+//		h.setLastName(lastName);
+		return h;
+		
 	}
 }
