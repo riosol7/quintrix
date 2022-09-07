173c4af6f202d00ded1c4019700e945ff70493c5
Pirai Ojeda (Rio) <87847753+riosol7@users.noreply.github.com>
2022-09-07 15:20:26 -0700

    HW6 structure tweak

--- demoServer/src/main/java/com/example/demo/controller/HelloController.java
+++ demoServer/src/main/java/com/example/demo/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.demo.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,17 +8,18 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestBody;
 //import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import com.example.demo.model.Employee;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import com.example.demo.Employee;
+//import com.example.demo.Employee;
 
 @RestController
 public class HelloController {
@@ -39,7 +40,7 @@ public class HelloController {
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(MediaType.APPLICATION_JSON);
 		
-		Employee ee = new Employee("test");
+		Employee ee = new Employee("test5");
 		
 		String json = null;
 		try {
