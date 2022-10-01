888c156d055f6beb9aabc4c03e1815d974c2c931
Pirai Ojeda (Rio) <87847753+riosol7@users.noreply.github.com>
2022-09-30 17:30:43 -0700

    hw14 typescript-angular init

--- demoEureka/src/main/java/com/demo/DemoEurekaApplication.java
+++ demoEureka/src/main/java/com/demo/DemoEurekaApplication.java
@@ -11,5 +11,4 @@ public class DemoEurekaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoEurekaApplication.class, args);
 	}
-
 }
--- demoNode/hw13.js
+++ demoNode/hw13.js
@@ -1,22 +1,22 @@
-// import axios from "axios";
+import axios from "axios";
 
-// const get = () => {
-//     axios.get('http://localhost:8080/employee')
-//     .then(res => {
-//         const info = res.data;
-//         console.log(`GET info: `, info);
-//     })
-//     .catch(err => console.log(err));
-// }
+const get = () => {
+    axios.get('http://localhost:8080/employee')
+    .then(res => {
+        const info = res.data;
+        console.log(`GET info: `, info);
+    })
+    .catch(err => console.log(err));
+}
 
-// get();
+get();
 
 //Use JS async function to read file
-const fs = require('fs');
+// const fs = require('fs');
 
-try{
-    const file = 'test.txt';
-    console.log(fs.readFileSync(file, 'utf-8'));
-} catch (err) {
-    console.log(err)
-}
+// try{
+//     const file = 'test.txt';
+//     console.log(fs.readFileSync(file, 'utf-8'));
+// } catch (err) {
+//     console.log(err)
+// }
